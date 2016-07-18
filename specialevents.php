7a4d4dda7cda7c9f1d5dd78926bb45bdc0f46d87
Dale Peakall <dpeakall@thoughtworks.com>
2016-07-18 19:35:27 +0100

    removed expired special events

--- specialevents.php
+++ specialevents.php
@@ -28,19 +28,7 @@
 
    <div id="content">
 
-   <h1>Carnival</h1>
-
-   <p><a href="https://winsterwakes.wordpress.com/">Winster Wakes and Carnival</a> runs from Sunday the 26th June to Saturday 2nd July and The Old Bowling Green will be hosting several special events during the week.</p>
-
-   <p><b>Tuesday, 28th June at 9pm</b> Allan's Carnival Quiz. Come along and join this fun general knowledge quiz - teams of up-to 4, &pound;2 per team.</p>
-
-   <p><b>Thursday, 30th June at 6pm</b> BBQ.  Cheer home the brave souls who have run Winster Hill Race with a Burger or Hot Dog in hand.</p>
- 
-   <p><b>Thursday, 30th June at 9pm</b> Shaun's Music Quiz. Test your musical knowledge at Shaun's famous quiz. Expect questions on music found in recent adverts and the next lines (sing-along) round. Maximum of 4 per team, &pound;2 per team.</p>
-
-   <h1>Secret Gardens</h1>
-
-   <p><a href="http://www.winster.org/pages/secret-gardens-of-winster.htm">The Secret Gardens of Winster</a> will be on Saturday 16th July and Sunday 17th July. The Old Bowling Green will be open for drinks at lunch-time on Saturday 16th (and all evening) and open as normal on Sunday 17th.</p>
+   <p>Check back for further special events later in the year.</p>
 
    </div>  
 
