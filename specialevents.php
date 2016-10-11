ddb8ee57c44ab8344a67e6763fe63cd4d835972d
Dale Peakall <dpeakall@thoughtworks.com>
2016-10-11 21:30:45 +0100

    Allan's Helen's Trust Quiz Night

--- specialevents.php
+++ specialevents.php
@@ -27,8 +27,55 @@
    <div id="contenttitle"><div class="header">Special Events</div></div>
 
    <div id="content">
+ 
+   <h1>Allan's Helen's Trust Quiz Night &ndash; 25th October</h1>
 
-   <p>Check back for further special events later in the year.</p>
+   <p>Our Second Helen's Trust Quiz will follow the format of the first one
+      &ndash; Allan's Quiz starts at 7:30pm and we will open the doors at
+      6:30pm; pie & pea supper served from 8:15pm and the quiz then resumes
+      at 9pm to finish around 9:45pm.  As it's a Tuesday we are opening
+      especially and only for the quiz.</p>
+
+   <p>As per last year, we are donating the entire takings from the evening
+      to Helen's Trust, and we are hoping that someone will come along and
+      say a few words on behalf of this most worthwhile charity &ndash; and
+      the ladies are once again organising their raffle which is already
+      being promoted in the Village Shop.</p>
+
+   <p>Last year we hoped for &pound;500 and were stunned when we reached
+      double that &ndash; we have great hopes that we can match it again
+      this year &ndash; <b>but we do need your support.</b></p>
+
+   <h2 class="heading">What do you have to do now?</h2>
+
+   <p>Register your interest as soon as possible and choose your pie either
+      by email or calling in.  Choice of pie:
+      <ul>
+        <li>Chicken &amp; Mushroom</li>
+        <li>Steak in Ale</li>
+        <li>Cheese &amp; Onion</li>
+      </ul>
+      (all served with mushy peas, chips &amp; gravy).</p>
+
+   <p>If your register by email it would be helpful if you called in and
+      paid your &pound;5 per head before the night.</p>
+
+   <p>If you just want to come to the quiz without the supper you are very
+      welcome &ndash; it's still a fiver though!</p>
+
+   <p>And &ndash; if you cannot come along but wish to support us please
+      feel free to drop in a donation.</p>
+
+   <p>As per last year we will suspend Green Club Prices on the night too.</p>
+
+   <p>Staff who come in have volunteered their time &ndash; please do support
+      us.  We are offering places to our Green Club first and only if we don't
+      fill up will we open it up to all comers.</p>
+
+   <p>A huge thank you to Allan for once again preparing the quiz.  Team of up
+      to four will be allowed, and you may bring a guest.</p>
+
+   <p>Look forward to seeing you on the night.</p>
 
    </div>  
 
